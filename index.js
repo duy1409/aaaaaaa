dbb26ad53b292cd0c7f1b62266f08d4207350e95
duy1409 <52957809+duy1409@users.noreply.github.com>
2020-03-31 03:44:18 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express();
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.set("views", "./views");
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 5432);
 var pg = require('pg');
 var config = {
 	user: 'lxfkuxyihgnmcj',
