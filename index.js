62f15fc87fa4939e2591d6780ea1b163c36a48c4
duy1409 <52957809+duy1409@users.noreply.github.com>
2020-03-31 22:48:02 +0700

    ccccc

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ var multer = require('multer');
 
 var storage = multer.diskStorage({
 	destination: function (req, file, cb){
-		cb(null, './public/avatar')
+		cb(null, '/public/avatar')
 	},
 	filename: function (req, file, cb) {
 		cb(null, file.originalname)
