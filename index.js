4bb5a34efa055fe1bd6972940c47cf97f14c6888
duy1409 <52957809+duy1409@users.noreply.github.com>
2020-03-31 19:21:49 +0700

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ var multer = require('multer');
 
 var storage = multer.diskStorage({
 	destination: function (req, file, cb){
-		cb(null, 'C:/New folder/aaaaaaa/upload')
+		cb(null, './upload')
 	},
 	filename: function (req, file, cb) {
 		cb(null, file.originalname)
