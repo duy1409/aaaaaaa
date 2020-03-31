2b75a48d1bec9d9d4c8a534e22189d36829e4084
duy1409 <52957809+duy1409@users.noreply.github.com>
2020-03-31 12:58:01 +0700

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,8 @@ var config = {
 	idleTimeoutMillis: 30000,
 
 };
-var pool = new pg.pool(config);
+
+var pool = new pg.Pool(config);
 
 app.get("/",function(req,res){
 	pool.connect(function(err, client, done){
