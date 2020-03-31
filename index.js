a3ffd09275c5b7541f93e8e3eb7f7e793e120ea1
duy1409 <52957809+duy1409@users.noreply.github.com>
2020-03-31 20:04:28 +0700

    xzcxzcxzc

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ var multer = require('multer');
 
 var storage = multer.diskStorage({
 	destination: function (req, file, cb){
-		cb(null, './public/upload')
+		cb(null, './public/images')
 	},
 	filename: function (req, file, cb) {
 		cb(null, file.originalname)
@@ -32,7 +32,7 @@ var storage = multer.diskStorage({
 
 })
 
-var upload = multer({ storage: storage }).single('uploadfile');
+var images = multer({ storage: storage }).single('uploadfile');
 
 var pool = new pg.Pool(config);
 
@@ -101,7 +101,7 @@ app.get("/product/add", function(req,res){
 });
 
 app.post("/product/add",urlencodeParser, function(req,res){
-	upload(req, res, function(err){
+	images(req, res, function(err){
 		if (err){
 			res.send("loi");
 		}else{
